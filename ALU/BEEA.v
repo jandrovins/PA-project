31cf9c3c888ecef503c7aebc9420550a93bc1bd1
Daniel Trujillo Viedma <dtviedma@gmail.com>
2023-11-25 13:59:36 +0100

    Improve BEEA: Make internal registers of 33 bits to accomodate the intermediate values

--- ALU/BEEA.v
+++ ALU/BEEA.v
@@ -8,15 +8,15 @@ module beea(
 	output signed [31:0] outC,
 	output rdy);
 
-	reg signed [31:0] curU, curV, curA, curC, curP;
-	wire [31:0] newU, newA, newV, newC;
-	reg signed [31:0] outCReg;
+	reg signed [32:0] curU, curV, curA, curC, curP;
+	wire [32:0] newU, newA, newV, newC;
+	reg signed [32:0] outCReg;
 	reg [1:0] status;
 	reg pifSelect;
 	wire pifRdyA, pifRdyB;
 
 	assign rdy = status == 2'b00;
-	assign outC = outCReg;
+	assign outC = {outCReg[32], outCReg[30:0]};
 
 	processIfEven instA (/*Inputs:*/ .clk(clk), .opselect(pifSelect), .u(curU), .a(curA), .p(curP), /*Outputs:*/ .outU(newU), .outA(newA), .rdy(pifRdyA));
 	processIfEven instB (/*Inputs:*/ .clk(clk), .opselect(pifSelect), .u(curV), .a(curC), .p(curP), /*Outputs:*/ .outU(newV), .outA(newC), .rdy(pifRdyB));
@@ -41,43 +41,63 @@ module beea(
 		pifSelect = 1'b0;
 	end
 
-	always @(posedge clk) begin
-		if(status == 2'b00) begin // Not operating
-			if(opselect) begin // Requested to operate, start operation
-				curU = k;
-				curV = p;
-				curA = 32'b1;
-				curC = 32'b0;
-				curP = p;
-				status = 2'b01;
-			end
-			// If there's no request to operate, do nothing.
-		end else if(status == 2'b01) begin // Operating, phase 1: working
-			if(curU == 32'b0) begin // Stop condition
-				outCReg = curC[31] == 1 ? curC + curP : curC;
-				status = 2'b00;
+	always @(posedge opselect) begin
+		curU <= {k[31], k};
+		curV <= {p[31], p};
+		curA <= 33'b1;
+		curC <= 33'b0;
+		curP <= {p[31], p};
+		status <= 2'b01;
+		pifSelect <= 1'b1;
+		#1 pifSelect <= 1'b0;
+	end
+
+	always @(posedge pifRdyB) begin
+		if(status == 2'b01 && pifRdyA && pifRdyB) begin
+			status = 2'b10;
+			if(newU >= newV) begin
+				curU <= newU - newV;
+				curV <= newV;
+				curA <= newA - newC;
+				curC <= newC;
 			end else begin
-				pifSelect = 1'b1;
-				//#2 pifSelect <= 1'b0;
-				#1 pifSelect = 1'b0;
-				status = 2'b10;
+				curU <= newU;
+				curV <= newV - newU;
+				curA <= newA;
+				curC <= newC - newA;
+			end // else: !if(newU >= newV)
+		end // if (pifRdyA && pifRdyB)
+	end // always @ (posedge pifRdyA, posedge pifRdyB)
+
+	always @(posedge pifRdyA) begin
+		if(status == 2'b01 && pifRdyA && pifRdyB) begin
+			status = 2'b10;
+			if(newU >= newV) begin
+				curU <= newU - newV;
+				curV <= newV;
+				curA <= newA - newC;
+				curC <= newC;
+			end else begin
+				curU <= newU;
+				curV <= newV - newU;
+				curA <= newA;
+				curC <= newC - newA;
+			end // else: !if(newU >= newV)
+		end // if (pifRdyA && pifRdyB)
+	end // always @ (posedge pifRdyA, posedge pifRdyB)
+
+
+	always @(posedge clk) begin
+		if(status == 2'b10) begin // Operating, phase 1: working
+			if(curU == 32'b0) begin // Stop condition
+				outCReg <= curC[31] == 1 ? curC + curP : curC;
+				status <= 2'b00;
+			end else begin
+				status <= 2'b01;
+				pifSelect <= 1'b1;
+				#1 pifSelect <= 1'b0;
 			end
-		end else if(status == 2'b10) begin // Operating, phase 2: Waiting for PIF
-			if(pifRdyA && pifRdyB) begin
-				status = 2'b01;
-				if(newU >= newV) begin
-					curU = newU - newV;
-					curV = newV;
-					curA = newA - newC;
-					curC = newC;
-				end else begin
-					curU = newU;
-					curV = newV - newU;
-					curA = newA;
-					curC = newC - newA;
-				end
-			end // if (pifRdyA && pifRdyB)
-		end // if (status == 2'b10)
+		end
 	end // always @(posedge clk)
 endmodule
 
@@ -86,11 +106,11 @@ endmodule
 module processIfEven(
 	input clk,
 	input opselect,
-	input [31:0] u,
-	input [31:0] a,
-	input [31:0] p,
-	output [31:0] outU,
-	output [31:0] outA,
+	input signed [32:0] u,
+	input signed [32:0] a,
+	input [32:0] p,
+	output signed [32:0] outU,
+	output signed [32:0] outA,
 	output rdy);
 
 	reg processing;
@@ -123,9 +143,9 @@ module processIfEven(
 
 
 	always @(posedge opselect) begin
-		curU <= {u[31], u};
-		curA <= {a[31], a};
-		curP <= {p[31], p};
+		curU <= u;
+		curA <= a;
+		curP <= p;
 		processing <= 1'b1;
 	end
 
--- ALU/BEEA_tb.v
+++ ALU/BEEA_tb.v
@@ -22,165 +22,195 @@ module top_module ();
 		$dumpvars(0, ERROR);
 		$dumpvars(0, testNumber);
 		testNumber = 32'd0;
+		opselect = 1'b0;
 		ERROR = 0;
 	end // initial begin
 
 	always @(posedge clk) begin
 		if(rdy) begin
-		case(testNumber)
-		  0: begin
-			  testNumber = 32'd1;
-			  u = 32'd4;
-			  a = 32'd9;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  1: begin
-			  if(outU != 32'd7) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			$display("posedge rdy");
+			case(testNumber)
+			  0: begin
+				  $display("launching test 1");
+				  testNumber = 32'd1;
+				  u = 32'd4;
+				  a = 32'd9;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd2;
-			  u = 32'd8;
-			  a = 32'd49;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  2: begin
-			  if(outU != 32'd43) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  1: begin
+				  if(outU != 32'd7) begin
+					  $display("ERROR in test 1");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 2");
+				  testNumber = 32'd2;
+				  u = 32'd8;
+				  a = 32'd49;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd3;
-			  u = 32'd10;
-			  a = 32'd81;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  3: begin
-			  if(outU != 32'd73) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  2: begin
+				  if(outU != 32'd43) begin
+					  $display("ERROR in test 2");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 3");
+				  testNumber = 32'd3;
+				  u = 32'd10;
+				  a = 32'd81;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd4;
-			  u = 32'd12;
-			  a = 32'd121;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  4: begin
-			  if(outU != 32'd111) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  3: begin
+				  if(outU != 32'd73) begin
+					  $display("ERROR in test 3");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 4");
+				  testNumber = 32'd4;
+				  u = 32'd12;
+				  a = 32'd121;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd5;
-			  u = 32'd16;
-			  a = 32'd225;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  5: begin
-			  if(outU != 32'd211) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  4: begin
+				  if(outU != 32'd111) begin
+					  $display("ERROR in test 4");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 5");
+				  testNumber = 32'd5;
+				  u = 32'd16;
+				  a = 32'd225;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd6;
-			  u = 32'd20;
-			  a = 32'd361;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  6: begin
-			  if(outU != 32'd343) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  5: begin
+				  if(outU != 32'd211) begin
+					  $display("ERROR in test 5");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 6");
+				  testNumber = 32'd6;
+				  u = 32'd20;
+				  a = 32'd361;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd7;
-			  u = 32'd400;
-			  a = 32'd159201;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  7: begin
-			  if(outU != 32'd158803) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  6: begin
+				  if(outU != 32'd343) begin
+					  $display("ERROR in test 6");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 7");
+				  testNumber = 32'd7;
+				  u = 32'd400;
+				  a = 32'd159201;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = 32'd8;
-			  u = 32'd1154;
-			  a = 32'd1329409;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  8: begin
-			  if(outU != 32'd1328257) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  7: begin
+				  if(outU != 32'd158803) begin
+					  $display("ERROR in test 7");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 8");
+				  testNumber = 32'd8;
+				  u = 32'd1154;
+				  a = 32'd1329409;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd1154;
-			  a = 32'h7fffffff; // From now on, fixed at this value
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  9: begin
-			  if(outU != 32'd154455063) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  8: begin
+				  if(outU != 32'd1328257) begin
+					  $display("ERROR in test 8");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 9");
+				  testNumber = testNumber + 1;
+				  u = 32'd1154;
+				  a = 32'h7fffffff; // From now on, fixed at this value
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd897940;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  10: begin
-			  if(outU != 32'd1229258249) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  9: begin
+				  if(outU != 32'd154455063) begin
+					  $display("ERROR in test 9");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 10");
+				  testNumber = testNumber + 1;
+				  u = 32'd897940;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd873241;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  11: begin
-			  if(outU != 32'd1451116319) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  10: begin
+				  if(outU != 32'd1229258249) begin
+					  $display("ERROR in test 10");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 11");
+				  testNumber = testNumber + 1;
+				  u = 32'd873241;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd1000;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  12: begin
-			  if(outU != 32'd36507222) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  11: begin
+				  if(outU != 32'd1451116319) begin
+					  $display("ERROR in test 11");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 12");
+				  testNumber = testNumber + 1;
+				  u = 32'd1000;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd7;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  13: begin
-			  if(outU != 32'd1840700269) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  12: begin
+				  if(outU != 32'd36507222) begin
+					  $display("ERROR in test 12");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 13");
+				  testNumber = testNumber + 1;
+				  u = 32'd7;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  testNumber = testNumber + 1;
-			  u = 32'd12865783;
-			  opselect = 1;
-			  #1 opselect = 0;
-		  end
-		  14: begin
-			  if(outU != 32'd224318682) begin
-				  ERROR = 1;
-				  #1 ERROR = 0;
+			  13: begin
+				  if(outU != 32'd1840700269) begin
+					  $display("ERROR in test 13");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  $display("launching test 14");
+				  testNumber = testNumber + 1;
+				  u = 32'd12865783;
+				  opselect = 1;
+				  #1 opselect = 0;
 			  end
-			  #2 $finish;
-		  end
-		endcase // case (testNumber)
+			  14: begin
+				  if(outU != 32'd224318682) begin
+					  $display("ERROR in test 14");
+					  ERROR = 1;
+					  #1 ERROR = 0;
+				  end
+				  #2 $finish;
+			  end
+			endcase // case (testNumber)
 		end // if (rdy)
 	end
 
