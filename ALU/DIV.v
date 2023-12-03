80f43aafad2526d32a11076db17faac5b0f2f4a4
Daniel Trujillo Viedma <dtviedma@gmail.com>
2023-12-03 01:27:29 +0100

    New DIV implementation with subtractions

--- ALU/DIV.v
+++ ALU/DIV.v
@@ -1,56 +1,78 @@
 
 
 module DIV(
-	input clk,
-	input reset,
-	input start,
-	input [30:0] a,
-	input [30:0] b,
-	output [30:0] _div,
-	output _rdy);
+	   input clk,
+	   input reset,
+	   input start,
+	   input signedness,
+	   input [31:0] a,
+	   input [31:0] b,
+	   output [31:0] q,
+	   output [31:0] r,
+	   output rdy);
 
-	reg processing;
-	wire nextProcessing;
-	reg [30:0] _divReg, latchedA;
-	wire [30:0] next_divReg, nextLatchedA;
-	wire [31:0] outInv;
-	wire rdyInv;
-	reg [63:0] mult;
-	wire [63:0] nextMult, newMult;
-	wire [31:0] newU2, newU3, newU4, newU41, newU42;
+	reg processing, error, qSign, rSign;
+	wire nextProcessing, nextError, stopCondition, nextQSign, nextRSign;
 
-	assign _rdy = processing == 0 ? ~nextProcessing : ~processing;
-	assign _div = _divReg;
+	reg [31:0]      dividend,     divisor,     quotient,     remainder;
+	wire [31:0] nextDividend, nextDivisor, nextQuotient, nextRemainder;
 
-	BEEA inst (.clk(clk), .reset(reset), .start(start), .k({1'b0, b}), .p(32'h7fffffff), .outC(outInv), .rdy(rdyInv));
 
-	assign newMult = mult * { {32{outInv[31]}}, outInv};
-	assign newU2 = {1'b0, newMult[30:0]};
-	assign newU3 = {1'b0, newMult[61:31]};
-	assign newU41 = newU2 + newU3;
-	assign newU42 = {1'b0, newU41[30:0]} + 1;
-	assign newU4 = newU42 == 32'h7fffffff ? 32'b0 :
-		       (newU41 > 32'h7fffffff ? newU42 : newU41 );
 
-	assign nextProcessing = start ? 1 :
-				processing && rdyInv ? 0 : processing;
+	assign q = qSign && signedness ? -quotient : quotient;
+	assign r = rSign && signedness ? -remainder : remainder;
+	assign rdy = ~processing;
 
-	assign nextLatchedA = start ? a : latchedA;
+	assign nextQSign = start ? (a[31] == 1) ^ (b[31] == 1) : qSign;
+	assign nextRSign = start ? a[31] : rSign;
 
-	assign nextMult = start ? {{33{a[30]}},a} : mult;
+	assign nextSameStart = !start && !processing ? 0 : 1;
 
-	assign next_divReg = start ? _divReg :
-			     processing && rdyInv ? newU4[30:0] : _divReg;
+
+	assign nextDividend = start && !signedness ? a :
+			      start && signedness ? (a[31] == 1 ? -a : a) :
+			      processing && !stopCondition ? dividend - divisor :
+			      dividend;
+	assign nextDivisor = start && !signedness ? b :
+			     start && signedness ? (b[31] == 1 ? -b : b) :
+			     divisor;
+	assign nextQuotient = start ? 0 :
+			      error ? -1 :
+			      processing && !stopCondition ? quotient + 1 :
+			      quotient;
+	assign nextRemainder = error ? 0 :
+			       processing && stopCondition ? dividend :
+			       remainder;
+
+	assign stopCondition = processing && error ? 1 :
+			       !processing ? 0 :
+			       divisor > dividend;
+	assign nextProcessing = start || (error || stopCondition ? 0 : processing);
+	assign nextError = start ? nextDivisor == 32'b0 :
+			   error;
 
 	always @(posedge clk, posedge reset) begin
 		if (reset) begin
 			processing <= 1'b0;
+			error <= 1'b0;
+			dividend <= 32'b0;
+			divisor <= 32'b1;
+			quotient <= 32'b0;
+			remainder <= 32'b0;
+			qSign <= 1'b0;
+			rSign <= 1'b0;
 		end else begin
-			latchedA <= nextLatchedA;
-			mult <= nextMult;
-			_divReg <= next_divReg;
+			dividend <= nextDividend;
+			divisor <= nextDivisor;
+			quotient <= nextQuotient;
+			remainder <= nextRemainder;
 			processing <= nextProcessing;
+			error <= nextError;
+			qSign <= nextQSign;
+			rSign <= nextRSign;
 		end
 	end
 
-endmodule
+
+endmodule // DIV
+
--- ALU/DIV_tb.v
+++ ALU/DIV_tb.v
@@ -2,42 +2,106 @@ module top_module ();
 	reg clk=0;
 	always #1 clk = ~clk;  // Create clock with period=2
 
-	reg divStart, reset;
-	reg [30:0] a, b;
-	wire [30:0] outDiv;
+	reg divStart, reset, signedness;
+	reg signed [31:0] a, b;
+	wire signed [31:0] outDiv, outRem;
         wire rdy;
 	reg [31:0] testNumber;
 	reg ERROR;
 
-	DIV inst (.clk(clk), .reset(reset), .start(divStart), .a(a), .b(b), ._div(outDiv), ._rdy(rdy));
+	DIV inst (.clk(clk), .reset(reset), .start(divStart), .signedness(signedness), .a(a), .b(b), .q(outDiv), .r(outRem), .rdy(rdy));
 
 	integer numTests, remainingTests, lastRemainingTestsReported;
+	integer testDataFd, bytesRead, exitLoop, expectedDiv, expectedRem;
+	reg[8*1000:0] comment;
 	initial begin
 		$dumpvars(0, top_module);
 		divStart = 1'b0;
 		reset = 1;
 		ERROR = 0;
 		#1 reset = 0;
-		numTests = 20000;
+
+		$write("Loading test data file: DIV_td.csv\n");
+		testDataFd = $fopen("DIV_td.csv", "r");
+		if (testDataFd == 0) begin
+			$write("ERROR. Data file not found\n");
+			$finish;
+		end
+		exitLoop = 0;
+		while (!exitLoop && ! $feof(testDataFd)) begin
+			#20 bytesRead = $fscanf(testDataFd, "%d,%d,%d,%d,%d\n", a, b, signedness, expectedDiv, expectedRem);
+			if (bytesRead > 0) begin
+				$write("Starting test: a = %0d, b = %0d, signedness = %0d. Expected a/b = %0d, a%%b = %0d... ", a, b, signedness, expectedDiv, expectedRem);
+				// TODO: Como divStart se levanta junto con clock, si el pulso es corto
+				// el módulo no pilla esta señal. Alguna solución mejor?
+				@(negedge clk) divStart = 1;
+				#2 divStart = 0;
+				@(posedge rdy) if(outDiv == expectedDiv && outRem == expectedRem) begin
+					$write("OK.\n");
+				end else begin
+					$write("ERROR. Actual a/b = %0d, a%%b = %0d\n", outDiv, outRem);
+					ERROR = 1;
+					#1 ERROR = 0;
+				end
+			end // else: !if(bytesRead == 0)
+		end
+		$fclose(testDataFd);
+		//$finish;
+
+
+
+
+		numTests = 2000;
 		$write("Start fuzzying. Will run %0d tests\n", numTests);
 		remainingTests = numTests;
 		lastRemainingTestsReported = remainingTests;
 		$write("%f%%\n", 0.0);
+		signedness = 1'b1;
+		while (remainingTests > numTests/2) begin
+			a = $random;
+			a = a % (2 ** 16);
+			b = $random;
+			// TODO: Como divStart se levanta junto con clock, si el pulso es corto
+			// el módulo no pilla esta señal. Alguna solución mejor?
+			@(posedge clk) divStart = 1;
+			#3 divStart = 0;
+			@(posedge rdy) if(b == 32'b0) begin
+				// These 2 ifs cannot be merged into 1, because is b is zero, we don't want the
+				// (outDiv * b + outRem != a) evaluated
+				if(outDiv != -1) begin
+					$write("ERROR test: a = %0d, b = 0. Expected quotient: -1, output quotient: %0d\n", a, outDiv);
+					ERROR = 1;
+					#1 ERROR = 0;
+				end
+			end else if (outDiv * b + outRem != a) begin
+				$write("ERROR test: a = %0d, b = %0d. Expected quotient & remainder: %0d, %0d, output quotient & remainder: %0d, %0d\n", a, b, a/b, a-(a/b)*b, outDiv, outRem);
+				ERROR = 1;
+				#1 ERROR = 0;
+			end
+			if (lastRemainingTestsReported - remainingTests >= 100) begin
+				lastRemainingTestsReported = remainingTests;
+				$write("%f%%\n", (numTests-remainingTests)*100.0/numTests);
+			end
+			remainingTests = remainingTests-1;
+		end // while (remainingTests > numTests/2)
 		while (remainingTests > 0) begin
 			a = $random;
-			a = a % (2**29);
 			b = $random;
-			b = b % (2**29);
-			while (b == 31'b0) begin
-				b = $random;
-				b = b % (2**29);
-			end
-			// TODO: Como divStart se levanta junto con clock a veces
-			// el módulo BEEA no pilla esta señal. Alguna solución mejor?
+			b = b % (2 ** 23);
+			// TODO: Como divStart se levanta junto con clock, si el pulso es corto
+			// el módulo no pilla esta señal. Alguna solución mejor?
 			divStart = 1;
 			#3 divStart = 0;
-			@(posedge rdy) if(((outDiv * b) % 2147483647) != a) begin
-				$write("ERROR test: a = %0d, b = %0d. Output: %0d\n", a, b, outDiv);
+			@(posedge rdy) if(b == 32'b0) begin
+				// These 2 ifs cannot be merged into 1, because is b is zero, we don't want the
+				// (outDiv * b + outRem != a) evaluated
+				if(outDiv != -1) begin
+					$write("ERROR test: a = %0d, b = 0. Expected quotient: -1, output quotient: %0d\n", a, outDiv);
+					ERROR = 1;
+					#1 ERROR = 0;
+				end
+			end else if (outDiv * b + outRem != a) begin
+				$write("ERROR test: a = %0d, b = %0d. Expected quotient & remainder: %0d, %0d, output quotient & remainder: %0d, %0d\n", a, b, a/b, a-(a/b)*b, outDiv, outRem);
 				ERROR = 1;
 				#1 ERROR = 0;
 			end
