ac05ecc1980ac6a744027b46b2ad756a14f4de12
Daniel Trujillo Viedma <dtviedma@gmail.com>
2023-12-17 00:45:40 +0100

    Implement a new version of the divisor, explicitly using a FSM. Clean the testbench, add some problematic test cases to the data file

--- ALU/DIV.v
+++ ALU/DIV.v
@@ -7,69 +7,96 @@ module DIV(
 	   input signedness,
 	   input [31:0] a,
 	   input [31:0] b,
-	   output [31:0] q,
-	   output [31:0] r,
-	   output rdy);
 
-	reg processing, error, qSign, rSign;
-	wire nextProcessing, nextError, stopCondition, nextQSign, nextRSign;
+	   output reg busy,
+	   output [31:0] q,
+	   output [31:0] r);
+
+	localparam [31:0] INITIAL_DIVISOR = 32'b1,
+			  INITIAL_DIVIDEND = 32'b0;
+
+	wire nextBusy;
+
+	reg      quotientSign,     remainingSign,     curSignedness;
+	wire nextQuotientSign, nextRemainingSign, nextCurSignedness;
 
 	reg [31:0]      dividend,     divisor,     quotient,     remainder;
 	wire [31:0] nextDividend, nextDivisor, nextQuotient, nextRemainder;
 
+	reg [3:0] status;
+	wire [3:0] nextStatus;
+	localparam [3:0] STATUS_IDLE = 3'b000,
+			 STATUS_LATCHING = 3'b001,
+			 STATUS_REDUCING = 3'b010,
+			 STATUS_COMPUTING = 3'b011,
+			 STATUS_ERROR = 3'b111;
 
 
-	assign q = qSign && signedness ? -quotient : quotient;
-	assign r = rSign && signedness ? -remainder : remainder;
-	assign rdy = ~processing;
+	assign q = quotientSign && curSignedness ? -quotient : quotient;
+	assign r = remainingSign && curSignedness ? -remainder : remainder;
 
-	assign nextQSign = start ? (a[31] == 1) ^ (b[31] == 1) : qSign;
-	assign nextRSign = start ? a[31] : rSign;
+	assign nextQuotientSign  = status == STATUS_LATCHING ? (a[31] == 1) ^ (b[31] == 1) : quotientSign;
+	assign nextRemainingSign = status == STATUS_LATCHING ? a[31] : remainingSign;
+	assign nextCurSignedness = status == STATUS_LATCHING ? signedness : curSignedness;
 
-	assign nextSameStart = !start && !processing ? 0 : 1;
+	// REDUCING PHASE DISABLED: The remainder must be fixed: shifted left as much times as the operands have been shifted right
+	assign nextStatus = status == STATUS_IDLE && start && b == 32'b0 ? STATUS_ERROR :
+			    status == STATUS_IDLE && start && b != 32'b0 ? STATUS_LATCHING :
 
+			    status == STATUS_LATCHING && nextDivisor <= nextDividend ? STATUS_COMPUTING :
+			    status == STATUS_LATCHING && nextDivisor > nextDividend ? STATUS_IDLE :
+			    // status == STATUS_LATCHING && (           a[0] == 0 &&           b[0] == 0) ? STATUS_REDUCING :
+			    // status == STATUS_LATCHING && (           a[0] == 1 ||           b[0] == 1) && nextDivisor <= nextDividend ? STATUS_COMPUTING :
+			    // status == STATUS_LATCHING && (           a[0] == 1 ||           b[0] == 1) && nextDivisor > nextDividend ? STATUS_IDLE :
 
-	assign nextDividend = start && !signedness ? a :
-			      start && signedness ? (a[31] == 1 ? -a : a) :
-			      processing && !stopCondition ? dividend - divisor :
+			    status == STATUS_REDUCING && (nextDividend[0] == 0 && nextDivisor[0] == 0) ? STATUS_REDUCING :
+			    status == STATUS_REDUCING && (nextDividend[0] == 1 || nextDivisor[0] == 1) && nextDivisor <= nextDividend ? STATUS_COMPUTING :
+			    status == STATUS_REDUCING && (nextDividend[0] == 1 || nextDivisor[0] == 1) && nextDivisor > nextDividend ? STATUS_IDLE :
+			    status == STATUS_COMPUTING && nextDivisor <= nextDividend ? STATUS_COMPUTING :
+			    STATUS_IDLE;
+
+	assign nextDividend = status == STATUS_LATCHING ? (curSignedness && a[31] == 1 ? -a : a) :
+			      status == STATUS_REDUCING ? dividend >>> 1 :
+			      status == STATUS_COMPUTING ? dividend - divisor :
+			      status == STATUS_ERROR ? 0 :
 			      dividend;
-	assign nextDivisor = start && !signedness ? b :
-			     start && signedness ? (b[31] == 1 ? -b : b) :
-			     divisor;
-	assign nextQuotient = start ? 0 :
-			      error ? -1 :
-			      processing && !stopCondition ? quotient + 1 :
-			      quotient;
-	assign nextRemainder = error ? 0 :
-			       processing && stopCondition ? dividend :
-			       remainder;
 
-	assign stopCondition = processing && error ? 1 :
-			       !processing ? 0 :
-			       divisor > dividend;
-	assign nextProcessing = start || (error || stopCondition ? 0 : processing);
-	assign nextError = start ? nextDivisor == 32'b0 :
-			   error;
+	assign nextDivisor = status == STATUS_LATCHING ? (curSignedness && b[31] == 1 ? -b : b) :
+			     status == STATUS_REDUCING ? divisor >>> 1:
+			     divisor;
+
+	assign nextQuotient = status == STATUS_IDLE && start ? 0 :
+			      status == STATUS_LATCHING ? 0 :
+			      status == STATUS_ERROR ? -1 :
+			      status == STATUS_REDUCING ? quotient :
+			      status == STATUS_COMPUTING ? quotient + 1 :
+			      quotient;
+
+	assign nextRemainder = dividend;
+
+	assign nextBusy = status != STATUS_IDLE;
 
 	always @(posedge clk, posedge reset) begin
 		if (reset) begin
-			processing <= 1'b0;
-			error <= 1'b0;
-			dividend <= 32'b0;
-			divisor <= 32'b1;
+			busy <= 1'b0;
+			dividend <= INITIAL_DIVIDEND;
+			divisor <= INITIAL_DIVISOR;
 			quotient <= 32'b0;
 			remainder <= 32'b0;
-			qSign <= 1'b0;
-			rSign <= 1'b0;
-		end else begin
+			quotientSign <= 1'b0;
+			remainingSign <= 1'b0;
+			curSignedness <= 1'b0;
+			status <= STATUS_IDLE;
+		end else begin // if (reset)
 			dividend <= nextDividend;
 			divisor <= nextDivisor;
 			quotient <= nextQuotient;
-			remainder <= nextRemainder;
-			processing <= nextProcessing;
-			error <= nextError;
-			qSign <= nextQSign;
-			rSign <= nextRSign;
+			remainder <= nextDividend;
+			busy <= nextBusy;
+			quotientSign <= nextQuotientSign;
+			remainingSign <= nextRemainingSign;
+			curSignedness <= nextCurSignedness;
+			status <= nextStatus;
 		end
 	end
 
--- ALU/DIV_tb.v
+++ ALU/DIV_tb.v
@@ -2,18 +2,22 @@ module top_module ();
 	reg clk=0;
 	always #1 clk = ~clk;  // Create clock with period=2
 
+	localparam testDataFileName = "DIV_td.csv",
+		   numTests = 2000;
+
 	reg divStart, reset, signedness;
 	reg signed [31:0] a, b;
 	wire signed [31:0] outDiv, outRem;
-        wire rdy;
+        wire busy;
 	reg [31:0] testNumber;
 	reg ERROR;
 
-	DIV inst (.clk(clk), .reset(reset), .start(divStart), .signedness(signedness), .a(a), .b(b), .q(outDiv), .r(outRem), .rdy(rdy));
+	DIV dut (.clk(clk), .reset(reset), .start(divStart), .signedness(signedness), .a(a), .b(b), .q(outDiv), .r(outRem), .busy(busy));
 
-	integer numTests, remainingTests, lastRemainingTestsReported;
+	integer remainingTests, lastRemainingTestsReported;
 	integer testDataFd, bytesRead, exitLoop, expectedDiv, expectedRem;
-	reg[8*1000:0] comment;
+	//reg [8*1000:0] null;
+
 	initial begin
 		$dumpvars(0, top_module);
 		divStart = 1'b0;
@@ -21,8 +25,8 @@ module top_module ();
 		ERROR = 0;
 		#1 reset = 0;
 
-		$write("Loading test data file: DIV_td.csv\n");
-		testDataFd = $fopen("DIV_td.csv", "r");
+		$write("Loading test data file: %s\n", testDataFileName);
+		testDataFd = $fopen(testDataFileName, "r");
 		if (testDataFd == 0) begin
 			$write("ERROR. Data file not found\n");
 			$finish;
@@ -35,37 +39,35 @@ module top_module ();
 				// TODO: Como divStart se levanta junto con clock, si el pulso es corto
 				// el módulo no pilla esta señal. Alguna solución mejor?
 				@(negedge clk) divStart = 1;
-				#2 divStart = 0;
-				@(posedge rdy) if(outDiv == expectedDiv && outRem == expectedRem) begin
+				@(posedge busy) divStart = 0;
+				@(negedge busy) if(outDiv == expectedDiv && outRem == expectedRem) begin
 					$write("OK.\n");
 				end else begin
 					$write("ERROR. Actual a/b = %0d, a%%b = %0d\n", outDiv, outRem);
 					ERROR = 1;
 					#1 ERROR = 0;
 				end
-			end // else: !if(bytesRead == 0)
+			end // else: !if(bytesRead > 0)
 		end
 		$fclose(testDataFd);
-		//$finish;
 
+		/* End of tests cases, begin fuzzying */
 
-
-
-		numTests = 2000;
 		$write("Start fuzzying. Will run %0d tests\n", numTests);
 		remainingTests = numTests;
 		lastRemainingTestsReported = remainingTests;
 		$write("%f%%\n", 0.0);
 		signedness = 1'b1;
-		while (remainingTests > numTests/2) begin
+		while (remainingTests > 0) begin
 			a = $random;
-			a = a % (2 ** 16);
+			a = (remainingTests > numTests/2) ? a % (2 ** 16) : a;
 			b = $random;
+			b = !(remainingTests > numTests/2) ? b % (2 ** 23) : b;
 			// TODO: Como divStart se levanta junto con clock, si el pulso es corto
 			// el módulo no pilla esta señal. Alguna solución mejor?
 			@(posedge clk) divStart = 1;
-			#3 divStart = 0;
-			@(posedge rdy) if(b == 32'b0) begin
+			@(posedge busy) divStart = 0;
+			@(negedge busy) if(b == 32'b0) begin
 				// These 2 ifs cannot be merged into 1, because is b is zero, we don't want the
 				// (outDiv * b + outRem != a) evaluated
 				if(outDiv != -1) begin
@@ -73,8 +75,12 @@ module top_module ();
 					ERROR = 1;
 					#1 ERROR = 0;
 				end
-			end else if (outDiv * b + outRem != a) begin
-				$write("ERROR test: a = %0d, b = %0d. Expected quotient & remainder: %0d, %0d, output quotient & remainder: %0d, %0d\n", a, b, a/b, a-(a/b)*b, outDiv, outRem);
+			end else if (outDiv != a/b) begin
+				$write("ERROR test: a = %0d, b = %0d. Expected quotient: %0d, output quotient: %0d\n", a, b, a/b, outDiv);
+				ERROR = 1;
+				#1 ERROR = 0;
+			end else if (outRem != a-(a/b)*b) begin
+				$write("ERROR test: a = %0d, b = %0d. Expected remainder: %0d, output remainder: %0d\n", a, b, a-(a/b)*b, outRem);
 				ERROR = 1;
 				#1 ERROR = 0;
 			end
@@ -83,34 +89,8 @@ module top_module ();
 				$write("%f%%\n", (numTests-remainingTests)*100.0/numTests);
 			end
 			remainingTests = remainingTests-1;
-		end // while (remainingTests > numTests/2)
-		while (remainingTests > 0) begin
-			a = $random;
-			b = $random;
-			b = b % (2 ** 23);
-			// TODO: Como divStart se levanta junto con clock, si el pulso es corto
-			// el módulo no pilla esta señal. Alguna solución mejor?
-			divStart = 1;
-			#3 divStart = 0;
-			@(posedge rdy) if(b == 32'b0) begin
-				// These 2 ifs cannot be merged into 1, because is b is zero, we don't want the
-				// (outDiv * b + outRem != a) evaluated
-				if(outDiv != -1) begin
-					$write("ERROR test: a = %0d, b = 0. Expected quotient: -1, output quotient: %0d\n", a, outDiv);
-					ERROR = 1;
-					#1 ERROR = 0;
-				end
-			end else if (outDiv * b + outRem != a) begin
-				$write("ERROR test: a = %0d, b = %0d. Expected quotient & remainder: %0d, %0d, output quotient & remainder: %0d, %0d\n", a, b, a/b, a-(a/b)*b, outDiv, outRem);
-				ERROR = 1;
-				#1 ERROR = 0;
-			end
-			if (lastRemainingTestsReported - remainingTests >= 100) begin
-				lastRemainingTestsReported = remainingTests;
-				$write("%f%%\n", (numTests-remainingTests)*100.0/numTests);
-			end
-			remainingTests = remainingTests-1;
-		end // while (numTests > 0)
+		end
+
 		$write("%f%%\n", 100.0);
 		$write("Done.\n");
 		$finish;
